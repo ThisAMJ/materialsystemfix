4d85bf3718a51f5365a40a848d86afd5a1c19361
AMJ <69196954+ThisAMJ@users.noreply.github.com>
2025-03-21 18:56:25 +1100

    fix: tolerate already-modified value

--- src/Modules/MaterialSystem.cpp
+++ src/Modules/MaterialSystem.cpp
@@ -10,28 +10,29 @@
 
 bool MaterialSystem::Init() {
 #ifdef _WIN32
-    auto sigSize = "BE 00 60 22 00";
+    auto sigSize = "56 BE ? ? ? ? E8";
     auto sigTerm = "FF 0D ? ? ? ? 75 ? B9";
     auto sigInit = "83 3D ? ? ? ? 00 0F 85 ? ? ? ? 56";
-    auto offSize = 1;
+    auto offSize = 2;
 #else
-    auto sigSize = "81 C3 00 60 22 00";
+    auto sigSize = "81 C3 ? ? ? ? E9";
     auto sigTerm = "83 2D ? ? ? ? 01 74 ? C3";
     auto sigInit = "A1 ? ? ? ? 85 C0 74 ? 83 C0 01";
     auto offSize = 2;
 #endif
-    // TODO: Use OLD_ALLOC to determine sigSize (00 60 22 00 = 0x226000)
     defaultContextSize = (uint32_t *)Memory::Scan(this->Name(), sigSize, offSize);
     RenderContextTerm = (_RenderContextTerm)Memory::Scan(this->Name(), sigTerm);
     RenderContextInit = (_RenderContextInit)Memory::Scan(this->Name(), sigInit);
     if (defaultContextSize && RenderContextTerm && RenderContextInit) {
-        Memory::UnProtect((void *)defaultContextSize, 4);
-        origContextSize = *defaultContextSize;
-        *defaultContextSize = NEW_ALLOC;
-
-        // OHGODPLEASEHELP: This is very jank. I'm sorry.
-        RenderContextTerm();
-        RenderContextInit();
+        if (*defaultContextSize != NEW_ALLOC) {
+            Memory::UnProtect((void *)defaultContextSize, 4);
+            origContextSize = *defaultContextSize;
+            *defaultContextSize = NEW_ALLOC;
+    
+            // OHGODPLEASEHELP: This is very jank. I'm sorry.
+            RenderContextTerm();
+            RenderContextInit();
+        }
         return this->hasLoaded = true;
     } else {
         console->Print("MSF: Failed to find material system signatures! %p %p %p\n", defaultContextSize, RenderContextTerm, RenderContextInit);
@@ -40,12 +41,10 @@ bool MaterialSystem::Init() {
 }
 
 void MaterialSystem::Shutdown() {
-    if (defaultContextSize) {
+    if (origContextSize) {
         *defaultContextSize = origContextSize;
-        if (RenderContextTerm && RenderContextInit) {
-            RenderContextTerm();
-            RenderContextInit();
-        }
+        RenderContextTerm();
+        RenderContextInit();
     }
 }
 
--- src/Plugin.hpp
+++ src/Plugin.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include "Utils.hpp"
 
-#define MSF_SIGNATURE "Material System Fix v1.0 by AMJ"
+#define MSF_SIGNATURE "Material System Fix v1.1 by AMJ"
 
 // CServerPlugin
 #define CServerPlugin_m_Size 16
